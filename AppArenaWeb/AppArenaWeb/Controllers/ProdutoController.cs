2c6e467680298e7792bae2cf61f673596442dec6
Pablo Soares <pabloti.soares@gmail.com>
2020-07-19 12:57:15 -0300

    Concluído o botão pesquisar do cadastro de produto faltando o incluir

--- AppArenaWeb/AppArenaWeb/Controllers/ProdutoController.cs
+++ AppArenaWeb/AppArenaWeb/Controllers/ProdutoController.cs
@@ -2,14 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppArenaWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppArenaWeb.Controllers
 {
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Index(ProdutoModel formulario)
+        {         
+            ProdutoModel objProduto = new ProdutoModel();
+            DepartamentoModel objDepartamento = new DepartamentoModel();
+            ViewBag.ListaDepartamentos = objDepartamento.ListaDepartamentos();
+            ViewBag.ListaProdutos = objProduto.ListaProdutos();
+            ViewBag.ListaProdutos = formulario.ListaProdutos();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProdutos(ProdutoModel formulario)
         {
+            if (ModelState.IsValid)
+            {
+                formulario.Sql_Insert_update();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarProduto(int? Id)
+        {
+            if (Id != null)
+            {
+                ProdutoModel objProduto = new ProdutoModel();
+                ViewBag.Registro = objProduto.CarregarRegistro(Id);             
+            }
+            ViewBag.ListaDepartamentos = new DepartamentoModel().ListaDepartamentos();
             return View();
         }
     }
--- AppArenaWeb/AppArenaWeb/Models/ProdutoModel.cs
+++ AppArenaWeb/AppArenaWeb/Models/ProdutoModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppArenaWeb.Util;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,116 @@ namespace AppArenaWeb.Models
 {
     public class ProdutoModel
     {
+        public int Id { get; set; }
+        [StringLength(199, MinimumLength = 3)]
+        [Required(ErrorMessage = "Informe a Descrição do produto com tamanho maior que 3 e menor que 200!")]
+        public string Descricao { get; set; }
+        [StringLength(20, MinimumLength = 3)]
+        [Required(ErrorMessage = "Informe a Dimenção maior que 3 e menor que 20!")]
+        public string Dimensao { get; set; }
+        public int IdadeMaxima { get; set; }
+        public int Id_Departamento { get; set; }
+        public int Id_Fornecedor { get; set; }
+        public string Status { get; set; }        
+        public string Obs { get; set; }
+
+        public ProdutoModel() 
+        { 
+
+        }
+
+        public List<ProdutoModel> ListaProdutos() 
+        {
+            List<ProdutoModel> produtos = new List<ProdutoModel>();
+            ProdutoModel item;
+            string sql = "";
+
+
+            if (Descricao != null && Id == 0 && Id_Departamento == 0)// Pesquisar por descricao
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE DESCRICAO LIKE '%{Descricao}%' ORDER BY DESCRICAO ASC";
+            }
+            else if (Descricao == null && Id != 0 && Id_Departamento == 0)// Pesquisar pelo codigo
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE ID = '{Id}' ORDER BY DESCRICAO ASC";
+            }
+            else if (Descricao != null && Id != 0 && Id_Departamento == 0)
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE ID = '{Id}' ORDER BY DESCRICAO ASC";
+            }
+            else if (Id_Departamento != 0 && Descricao != null && Id == 0)
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE DESCRICAO LIKE '%{Descricao}%' AND ID_DEPARTAMENTO = '{Id_Departamento}' ORDER BY DESCRICAO ASC";
+            }
+            else if (Id_Departamento != 0 && Descricao == null && Id != 0)
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE ID = '{Id}' AND ID_DEPARTAMENTO = '{Id_Departamento}' ORDER BY DESCRICAO ASC";
+            }
+            else if (Id_Departamento != 0 && Descricao == null && Id == 0) 
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE ID_DEPARTAMENTO = '{Id_Departamento}' ORDER BY DESCRICAO ASC";
+            }
+            else if (Id_Departamento != 0 && Descricao != null && Id != 0)
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE ID_DEPARTAMENTO = '{Id_Departamento}' AND ID = '{Id}' AND DESCRICAO LIKE '%{Descricao}%'  ORDER BY DESCRICAO ASC";
+            }
+            else
+            {
+                sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE DESCRICAO LIKE '%{Descricao}%' ORDER BY DESCRICAO ASC";
+            }
+
+            DAL objDal = new DAL();
+            DataTable dt = objDal.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new ProdutoModel();
+                item.Id = int.Parse(dt.Rows[i]["ID"].ToString());
+                item.Descricao = dt.Rows[i]["DESCRICAO"].ToString();
+                item.Dimensao = dt.Rows[i]["DIMENSAO"].ToString();
+                item.IdadeMaxima = int.Parse(dt.Rows[i]["IDADE_MAXIMA"].ToString());
+                item.Id_Departamento = int.Parse(dt.Rows[i]["ID_DEPARTAMENTO"].ToString());
+                item.Id_Fornecedor = int.Parse(dt.Rows[i]["ID_FORNECEDOR"].ToString());
+                item.Status = dt.Rows[i]["STATUS"].ToString();
+                item.Obs = dt.Rows[i]["OBS"].ToString();                
+                produtos.Add(item);
+            }
+            return produtos;
+        }
+
+        public ProdutoModel CarregarRegistro(int? Id) 
+        {
+            ProdutoModel item = new ProdutoModel();
+            string sql = $"SELECT ID, DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS FROM PRODUTO WHERE ID = '{Id}' ORDER BY DESCRICAO ASC";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            item = new ProdutoModel();
+            item.Id = int.Parse(dt.Rows[0]["ID"].ToString());
+            item.Descricao = dt.Rows[0]["DESCRICAO"].ToString();
+            item.Dimensao = dt.Rows[0]["DIMENSAO"].ToString();
+            item.IdadeMaxima = int.Parse(dt.Rows[0]["IDADE_MAXIMA"].ToString());
+            item.Id_Departamento = int.Parse(dt.Rows[0]["ID_DEPARTAMENTO"].ToString());
+            item.Id_Fornecedor = int.Parse(dt.Rows[0]["ID_FORNECEDOR"].ToString());
+            item.Status = dt.Rows[0]["STATUS"].ToString();
+            item.Obs = dt.Rows[0]["OBS"].ToString();
+            return item;
+        }
+
+        public void Sql_Insert_update()
+        {
+            string sql = "";
+
+            if (Id == 0)
+            {
+                sql = $"INSERT INTO PRODUTO (DESCRICAO, DIMENSAO, IDADE_MAXIMA, ID_DEPARTAMENTO, ID_FORNECEDOR, STATUS, OBS) VALUES ('{Descricao}', '{Dimensao}', '{IdadeMaxima}', '{Id_Departamento}', '{Id_Fornecedor}','{Status}','{Obs}');";
+            }
+            else
+            {
+                sql = $"UPDATE CLIENTE SET DESCRICAO = '{Descricao}', DIMENSAO = '{Dimensao}', IDADE_MAXIMA = '{IdadeMaxima}', ID_DEPARTAMENTO = '{Id_Departamento}', ID_FORNECEDOR = '{Id_Fornecedor}', STATUS = '{Status}', OBS = '{Obs}'  WHERE ID = '{Id}'";
+            }
+            DAL objDal = new DAL();
+            objDal.ExecutarComandoSQL(sql);
+        }
     }
 }
